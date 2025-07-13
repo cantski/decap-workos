d2ae011624219a25dfa7a602d8a3393f865a83cf
cantski <164589592+cantski@users.noreply.github.com>
2025-07-13 23:29:26 +1000

    Support images

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 export default async function (eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
   eleventyConfig.setOutputDirectory("dist");
+  eleventyConfig.addPassthroughCopy("src/images");
 }
--- src/posts/hello.md
+++ src/posts/hello.md
@@ -4,3 +4,5 @@ permalink: /posts/hello/
 ---
 
 Hey there
+
+![A Mr Satan superfan beckons from the statdium crowd](/images/satan-shirt.jpg)
