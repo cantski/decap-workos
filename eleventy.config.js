a7bd88eda9fab79e386b95e1f54996a211b5db54
cantski <164589592+cantski@users.noreply.github.com>
2025-07-13 23:50:31 +1000

    Add decap cms

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,4 +2,5 @@ export default async function (eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
   eleventyConfig.setOutputDirectory("dist");
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/admin/config.yml");
 }
