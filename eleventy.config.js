def1a6131e4bf6ea8305d27787f08e3062d83e92
cantski <164589592+cantski@users.noreply.github.com>
2025-07-16 18:48:56 +1000

    Use local copy of decap cms

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,10 @@
 export default async function (eleventyConfig) {
-  eleventyConfig.setInputDirectory("src");
-  eleventyConfig.setOutputDirectory("dist");
-  eleventyConfig.addPassthroughCopy("src/images");
-  eleventyConfig.addPassthroughCopy("src/admin/config.yml");
+    eleventyConfig.setInputDirectory('src');
+    eleventyConfig.setOutputDirectory('dist');
+    eleventyConfig.addPassthroughCopy('src/images');
+    eleventyConfig.addPassthroughCopy('src/admin/config.yml');
+    eleventyConfig.addPassthroughCopy({
+        'node_modules/decap-cms/dist/decap-cms.js': 'js/decap-cms.js',
+        'node_modules/decap-cms/dist/decap-cms.js.map': 'js/decap-cms.js.map',
+    });
 }
